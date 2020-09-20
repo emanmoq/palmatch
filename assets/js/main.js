df1dd6931fe7a8b571103d7ce352e95f327e29f7
emanmoq <42995029+emanmoq@users.noreply.github.com>
2020-09-20 18:18:15 +0300

    dd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,23 @@
 jQuery(function ($) {
 
-
+    $('.successSlider').owlCarousel({
+        autoplay: true,
+        loop: false,
+        margin: 15,
+        responsive: {
+          0: {
+            items: 1,
+           margin:0
+  
+          },
+          600: {
+            items: 2,
+          },
+          1000: {
+            items: 3,
+          }
+        }
+      });
 
 });
 
